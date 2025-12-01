0e9c89288e6f40aa20d13571e0b536dbc1f6ef11
Francisco Ruvalcaba C <32653773+frantizek@users.noreply.github.com>
2025-12-01 11:45:19 -0600

    Initial version of 01_screenshot

---
+++ projects/01_screenshot/main.py
@@ -0,0 +1,40 @@
+# Goal: Master file I/O and System Interaction.
+# 1. The Auto-Screenshot Reminder
+# Concept: Infinite loops, timestamps, binary file writing.
+
+# Lesson learned: automation doesn't have to be big, it just has to be useful.
+
+# Personal notes:
+# This is an infinite loop, using CLI maybe using a button to start / stop would be best?
+# What happens when you have multiple monitors? What is being captured? Should we notify the user?
+# Maybe next versions will include using parameters to change the directory, add specific name,
+# and even reduce the time for the screenshots.
+
+
+import pyautogui
+import time
+import os
+from pathlib import Path
+
+DEFAULT_TIME_BETWEEN_SCREENSHOTS = 300  # Expressed in seconds, meaning 5 minutes.
+
+
+def main():
+    while True:
+        directory_storing_screenshots = "automated_screenshots"
+        my_dir = Path(directory_storing_screenshots)
+        try:
+            my_dir.mkdir()
+            print(f"Directory {directory_storing_screenshots} created.")
+        except FileExistsError:
+            print(f"Directory {directory_storing_screenshots} already exists.")
+
+        file_name = time.strftime("%Y-%m-%d_%H-%M-%S") + ".png"
+
+        pyautogui.screenshot(os.path.join("automated_screenshots", file_name))
+
+        time.sleep(DEFAULT_TIME_BETWEEN_SCREENSHOTS)
+
+
+if __name__ == "__main__":
+    main()
